be52595bb8788abcc7821c4b8f3485273ddecb24
Sukalyan <sukalyandash1920@gmail.com>
2022-02-02 12:20:13 +0530

    UC-2_ValidateLastName

--- UserRegistrationUsingLambda/RegeX.cs
+++ UserRegistrationUsingLambda/RegeX.cs
@@ -9,14 +9,21 @@ namespace UserRegistration_Lambda
 {
     internal class RegeX
     {
-
-
+        //UC-1
         //pattern for valid name
         public static string REGEX = "^[A-Z]{1}[A-Za-z]{2,}$";
 
         public bool ValidFirstName(string name)
         {
             return Regex.IsMatch(name, REGEX);
+        }
+        //UC-2
+        //pattern for valid name
+        public static string LastNameREGEX = "^[A-Z]{1}[A-Za-z]{2,}$";
+
+        public bool ValidLastName(string name)
+        {
+            return Regex.IsMatch(name, LastNameREGEX);
         }
 
     }
